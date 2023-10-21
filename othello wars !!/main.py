d12d64e589250a5e506fd87a8c20c5f695ec0cac
paozerx <146060605+paozerx@users.noreply.github.com>
2023-10-21 20:05:10 +0700

    Add files via upload

---
+++ othello wars !!/main.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+import customtkinter
+from othellogame import Othello
+
+
+def main():
+    program = Start()
+    program.window.mainloop()
+
+
+class Start :
+    def __init__(self):
+        self.window = customtkinter.CTk()
+        self.window.title("Othello Wars")
+        self.create_widgets()
+    def playgame(self):
+        self.window.destroy()
+        game = Othello()
+        game.game_start()
+
+    def create_widgets(self):
+        self.window.geometry("900x600+400+200")
+        
+        customtkinter. set_default_color_theme('green')
+        topic = customtkinter.CTkLabel(self.window,font=('courier',40,'bold'), text='Othello Wars').place(x=320, y=120)
+        pvp = customtkinter.CTkButton(self.window,font=('courier',24,'bold'), text='Player vs Player', command=self.playgame).place(x=150, y=250)
+        pva1 = customtkinter.CTkButton(self.window,font=('courier',24,'bold'), text='Player vs Mr.Jarvis').place(x=500, y=250)
+        pva2 = customtkinter.CTkButton(self.window,font=('courier',24,'bold'), text='Player vs Mr.Dose').place(x=150, y=390)
+        ava = customtkinter.CTkButton(self.window,font=('courier',24,'bold'), text='Mr.Jarvis vs Mr.Dose').place(x=500, y=390)
+    
+
+
+        
+        
+        
+
+if __name__ == '__main__':
+    main()
